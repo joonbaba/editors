d2d7059f419442cf943eb9b41000f4416d66f448
King <54558748+joonbaba@users.noreply.github.com>
2020-07-04 02:27:24 -0700

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { useEffect, useMemo, useState } from 'react'
+// Import the Slate editor factory.
+import { createEditor } from 'slate'
+// Import the Slate components and React plugin.
+import { Slate, Editable, withReact } from 'slate-react'
+import Images from './tools/images'
+import Home from './pages/page'
+import NoSsrExamplePage from './pages/page-docs'
+import RichText from './tools/richtext'
+
+const App = () => {
+	const editor = useMemo(() => withReact(createEditor()), [])
+	// Add the initial value when setting up our state.
+	const [value, setValue] = useState([
+		{
+			type: 'paragraph',
+			children: [{ text: 'A line of text in a paragraph.' }],
+		}
+	]as any)
+	return (
+		<Slate editor={editor} value={value} onChange={newValue => setValue(newValue)}>
+		<link
+			rel="stylesheet"
+			href="https://fonts.googleapis.com/css?family=Roboto:400,400i,700,700i&subset=latin-ext"
+		/>
+		<link
+		rel="stylesheet"
+		href="https://fonts.googleapis.com/icon?family=Material+Icons"
+		/>
+			{/* <Editable /> */}
+			<Images />
+			<RichText />
+			{/* <Home /> */}
+{/* <NoSsrExamplePage /> */}
+		</Slate>
+	)
 }
+export default App
 
-export default App;
